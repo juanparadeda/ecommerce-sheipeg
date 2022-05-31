1fd221aaa5bab682136ffe79d1574e4479b96d30
juanparadeda <juanparadeda@gmail.com>
2022-05-30 21:16:49 -0300

    improved 404 Not Found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button, Container } from "@mui/material";
+
 const NotFound = () => {
     return (
-        <h1>NOT FOUND</h1>
+        <>
+            <h1>404 - Página no encontrada</h1>
+            <h2>Ooooops... No encontramos la página que buscabas</h2>
+            <Link style={{ textDecoration: 'none', color: 'inherit' }} to="/">
+                <Button color="secondary" variant="contained" sx={{marginBottom: '20px'}}>
+                    Volver al Inicio
+                </Button>
+            </Link>
+            <Container maxWidth='sm' >
+                <img src="/brokencamera.jpg" />
+            </Container>
+
+        </>
     )
 }
 
