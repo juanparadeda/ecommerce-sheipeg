ae4bd477b858a56cac1d19360ce911a998ba6b24
juanparadeda <juanparadeda@gmail.com>
2022-06-11 14:40:26 -0300

    enhanced NavBar.js JSX code even more

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,13 +2,7 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import CartWidget from '../CartWidget/CartWidget';
 import './NavBar.scss';
 import { Link } from 'react-router-dom';
@@ -16,16 +10,11 @@ import HamburgerMenu from '../HamburgerMenu/HamburguerMenu';
 import DesktopNavButton from '../DesktopNavButton/DesktopNavButton';
 import Logo from '../Logo/Logo';
 import Brand from '../Brand/Brand';
+import UserMenu from '../UserMenu/UserMenu';
 
-const settings = ['Mi Perfil', 'Mis Órdenes', 'Logout'];
+
 const NavBar = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+
   return (
     <AppBar className='NavBar' position="sticky">
       <Container maxWidth="xl">
@@ -45,35 +34,7 @@ const NavBar = () => {
             </DesktopNavButton>
           </Box>
           <Link to='/cart'><CartWidget /></Link>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          <UserMenu />
         </Toolbar>
       </Container>
     </AppBar >
