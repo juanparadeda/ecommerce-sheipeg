186444d2fc4cc24713e8d865c7369b8e94f70edc
juanparadeda <juanparadeda@gmail.com>
2022-06-13 16:55:13 -0300

    removed mock file and js file with filter functions. Now categories are filtered in the query to FireStore

--- src/utils/filterProduct.js
+++
@@ -1,9 +0,0 @@
-
-
-const filterProductById = (arrayOfProducts, id) => {
-    return arrayOfProducts.find((product) => product.id === parseInt(id));
-}
-const filterProductsByCategory = (arrayOfProducts, category) => {
-    return arrayOfProducts.filter((product) => product.category === category)
-}
-export { filterProductById, filterProductsByCategory };
