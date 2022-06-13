6d10e65aeeafade45cebe6ddd9ae5973275d8ea8
juanparadeda <juanparadeda@gmail.com>
2022-06-13 15:40:18 -0300

    pulling products from FireBase FireStore

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -4,28 +4,39 @@ import { products } from '../../utils/productsMock.js';
 import ItemDetail from '../ItemDetail/ItemDetail.js';
 import {filterProductById}from "../../utils/filterProduct.js";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner.js";
+import { collection, doc, getDocs, query, where, getDoc } from "firebase/firestore";
+import db from "../../utils/firebaseConfig";
 
 const ItemDetailContainer = () => {
     const { id } = useParams();
     const [productState, setProductState] = useState({});
 
-    const getProduct = () => {
+    /*const getProduct = () => {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(filterProductById(products, id));
             }, 500);
         });
-    };
+    };*/
     useEffect(() => {
-        getProduct()
+        const getProductFromFirebase = async () =>{
+            const docRef = doc(db, 'products', id);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                let product = docSnap.data();
+                product.id = docSnap.id;
+                setProductState(product);
+            }
+        }
+        getProductFromFirebase();
+        /*getProduct()
             .then((response) => {
                 setProductState(response);
             })
             .catch((error) => {
                 console.log('ERROR');
-            })
+            })*/
     }, [])
-
     return (
         
         <>  
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,15 @@
-import { useEffect, useState, useContext } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
-import {  getProducts } from "../../utils/productsMock";
+import { getProducts } from "../../utils/productsMock";
 import { filterProductsByCategory } from "../../utils/filterProduct";
 import ItemList from "../ItemList/ItemList";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { collection, doc, getDocs, query, where, getDoc } from "firebase/firestore";
+import db from "../../utils/firebaseConfig";
 
 const ItemListContainer = () => {
     const [productsState, setProductsState] = useState([]);
-    const [SpinnerState, setSpinnerState] = useState({display: 'flex' })
+    const [SpinnerState, setSpinnerState] = useState({ display: 'flex' })
     const { category } = useParams();
 
     let title = '';
@@ -17,25 +19,73 @@ const ItemListContainer = () => {
     (category === 'accesorios') && (title = 'Accesorios');
     (category === undefined) && (title = 'Sheipeg | Tu Tienda de Fotografía') && (subtitle = 'Catálogo de Productos')
     useEffect(() => {
+
         setProductsState([]);
-        setSpinnerState({display: 'flex'});
-        getProducts()
-        .then((res) =>{
-            if (category === undefined) {
-                
-                setProductsState(res);
-            } else {
-            const filteredProducts = filterProductsByCategory(res, category);
-            setProductsState(filteredProducts);
+        setSpinnerState({ display: 'flex' });
+        /*getProducts()
+            .then((res) => {
+                if (category === undefined) {
+
+                    setProductsState(res);
+                } else {
+                    const filteredProducts = filterProductsByCategory(res, category);
+                    setProductsState(filteredProducts);
+                }
+            })
+            .catch((error) => {
+                console.log('ERROR');
+            })
+            .finally(() => {
+                setSpinnerState({ display: 'none' })
+            })*/
+
+        if (category === undefined) {
+            getProductsFromFireStore()
+                .then((res) => {
+                    setProductsState(res)
+                })
+                .finally(() => {
+                    setSpinnerState({ display: 'none' })
+                })
+        } else {
+            getProductsCategory(category)
+                .then((res) => {
+                    setProductsState(res)
+                })
+                .finally(() => {
+                    setSpinnerState({ display: 'none' })
+                })
+        }
+        const getProductFromFirebase = async () =>{
+            const docRef = doc(db, 'products', 'RaptGUKcG8Qe1i9Y4tg8');
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                console.log(docSnap.data())
             }
-        })
-        .catch((error) => {
-            console.log('ERROR');
-        })
-        .finally(() =>{
-            setSpinnerState({display: 'none'})
-        })
+        }
+        getProductFromFirebase();
     }, [category])
+
+    const getProductsFromFireStore = async () => {
+        const productSnapshot = await getDocs(collection(db, 'products'));
+        const productList = productSnapshot.docs.map((doc) => {
+            let product = doc.data();
+            product.id = doc.id;
+            return product;
+        })
+        
+        return (productList);
+    }
+    const getProductsCategory = async (category) => {
+        const q = query(collection(db, 'products'), where ('category', '==', category))
+        const categorySnapshot = await getDocs(q)
+        const categoryList = categorySnapshot.docs.map((doc) => {
+            let product = doc.data();
+            product.id = doc.id;
+            return product;
+        })
+        return categoryList;
+    }
     return (
         <>
             <h1>{title}</h1>
